773aa27d483c63918d21b3e9f9bb8f103d7f4850
AVTim95 <timutsa.an@gmail.com>
2023-05-15 23:12:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Семестровая работа
+
+Условия: https://github.com/hse-econ-data-science/andan_2023/blob/main/project_rules.md
---
+++ parser.py
@@ -0,0 +1,18 @@
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+caps = DesiredCapabilities().CHROME
+caps["pageLoadStrategy"] = "normal"
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options, desired_capabilities=caps)
+
+driver.get('https://raw.githubusercontent.com/Yorko/mlcourse.ai/main/data/Rail_Insurance_Claims.csv')
+time.sleep(1)
+
+with open('dataset.csv', 'w') as f:
+    f.write(driver.find_element(By.TAG_NAME, value='body').text)
